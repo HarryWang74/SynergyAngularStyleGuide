2ea76afb7edb9b5056c7dd4d1398e849496c63bd
Harry <harry74323@gmail.com>
2019-03-14 17:19:53 +1100

    add material columns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SynergyAngularStyleGuide';
+  
+
+  webColumns = {
+    totalColumns: 12,
+    height: '100px',
+    columnPadding:  '40px',
+    leftColumn: { cols: 8, color: 'lightblue'},
+    rightColumn: { cols: 4, color: 'lightgreen'}
+  };
+
+  tabletColumns = {
+    totalColumns: 12,
+    height: '100px',
+    columnPadding:  '10px',
+    leftColumn: { cols: 6, color: 'lightblue'},
+    rightColumn: { cols: 6, color: 'lightgreen'}
+  };
+
+  columns = this.webColumns
+
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.Web
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.columns = this.webColumns;
+
+      }
+    });
+
+    breakpointObserver.observe([
+      Breakpoints.Tablet
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.columns = this.tabletColumns;
+      }
+    });
+  }
+
+
 }
